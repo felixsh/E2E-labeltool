3d15849d8476c45a7a457680f9798c2fec81ca66
Felix Hauser <f.hauser@posteo.de>
2025-10-25 00:32:24 +0200

    Fix and clarify optimization cost function

--- src/optimizerCost.js
+++ src/optimizerCost.js
@@ -5,53 +5,40 @@ export function makeEvaluateCost({
 }) {
   return function evaluateCost(Tarray, dt, cfg) {
     const fixed = getFixedPoints ? getFixedPoints() : [];
-    let paramPoints = Tarray.map(t => {
+    const paramPoints = Tarray.map(t => {
       const p = getParamPoint(t);
       return [p.x, p.y];
     });
-    paramPoints = paramPoints.slice(1);    
-    const P = fixed.concat(paramPoints);
-
-    let jerk = 0;
-    for (let i = 0; i <= P.length - 4; i++) {
-      const jx = (P[i + 3][0] - 3 * P[i + 2][0] + 3 * P[i + 1][0] - P[i][0]);
-      const jy = (P[i + 3][1] - 3 * P[i + 2][1] + 3 * P[i + 1][1] - P[i][1]);
-      jerk += jx * jx + jy * jy;
+    
+    const P = fixed.concat(paramPoints.slice(1));
+    
+    const pointCount = P.length;
+    if (pointCount < 2) {
+      return 0;
     }
-
+    
+    const penV = d3.range(pointCount - 1).reduce((sum, i) => {
+      const vx = (P[i + 1][0] - P[i][0]) / dt;
+      const vy = (P[i + 1][1] - P[i][1]) / dt;
+      return sum + vx * vx + vy * vy;
+    }, 0);
+    
     const dt2 = dt * dt;
-    const v_ms = d3.range(P.length - 1).map(i => {
-      const dx = (P[i + 1][0] - P[i][0]) / dt;
-      const dy = (P[i + 1][1] - P[i][1]) / dt;
-      return Math.hypot(dx, dy);
-    });
-
-    const a = d3.range(P.length - 2).map(i => {
+    const penA = d3.range(pointCount - 2).reduce((sum, i) => {
       const ax = (P[i + 2][0] - 2 * P[i + 1][0] + P[i][0]) / dt2;
       const ay = (P[i + 2][1] - 2 * P[i + 1][1] + P[i][1]) / dt2;
-      return [ax, ay];
-    });
-    const tHat = d3.range(P.length - 2).map(i => {
-      const tx = P[i + 2][0] - P[i][0];
-      const ty = P[i + 2][1] - P[i][1];
-      const n = Math.hypot(tx, ty) || 1;
-      return [tx / n, ty / n];
-    });
-    const aLong = a.map(([ax, ay], i) => ax * tHat[i][0] + ay * tHat[i][1]);
-    const aLat = a.map(([ax, ay], i) => {
-      const al = aLong[i];
-      const axp = ax - al * tHat[i][0];
-      const ayp = ay - al * tHat[i][1];
-      const mag = Math.hypot(axp, ayp);
-      const sign = Math.sign(tHat[i][0] * ay - tHat[i][1] * ax) || 1;
-      return sign * mag;
-    });
+      return sum + ax * ax + ay * ay;
+    }, 0);
+    
+    const dt3 = dt2 * dt;
+    const penJ = pointCount >= 4
+      ? d3.range(pointCount - 3).reduce((sum, i) => {
+          const jx = (P[i + 3][0] - 3 * P[i + 2][0] + 3 * P[i + 1][0] - P[i][0]) / dt3;
+          const jy = (P[i + 3][1] - 3 * P[i + 2][1] + 3 * P[i + 1][1] - P[i][1]) / dt3;
+          return sum + jx * jx + jy * jy;
+        }, 0)
+      : 0;
 
-    const penV = v_ms.reduce((sum, v) => sum + v * v, 0);
-    const penA =
-      aLong.reduce((sum, val) => sum + val * val, 0) +
-      aLat.reduce((sum, val) => sum + val * val, 0);
-
-    return cfg.wJerk * jerk + cfg.wVel * penV + cfg.wAcc * penA;
+    return cfg.wJerk * penJ + cfg.wVel * penV + cfg.wAcc * penA;
   };
 }
